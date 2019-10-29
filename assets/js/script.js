5b75b1a1718a6ff84b872492441b7c27fc0c8aaa
Jerald Gutierrez <jerald.gutierrez@thinkconveyancing.com.au>
2019-10-30 06:47:42 +0800

    Commit js configuation

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,3 +25,23 @@ window.addEventListener("wheel", event => {
 });
 //when user scroll
 //new WOW().init();
+
+$(".home-accordion").find("[data-toggle='collapse']").click(function(event) {
+    var element = $(event.target).closest('.home-card-accordion');
+    // Check if body content is show
+    window.setTimeout(function(){
+        jQuery(".home-card-accordion").each(function() {
+            // Change icon
+            var eIsShow = jQuery(this).find(".collapse-body").hasClass("show");
+            if ( !eIsShow ) {
+                jQuery(this).find("i").removeClass("fa-angle-up").addClass("fa-angle-down");
+            }
+        });
+
+        var isShow = element.find(".collapse-body").hasClass("show");
+        if ( isShow ) {
+            // Change icon
+            element.find("i").removeClass("fa-angle-down").addClass("fa-angle-up");
+        }
+    },500);
+});
